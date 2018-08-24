e08943c95cae7b58743d766090ebc376a558f6a3
corozco <corozcor@elektra.com.mx>
2018-08-24 14:37:42 -0500

    Se agrega conversor de numero a letra

---
+++ src/main/java/com/estopacomplementos/core/business/Prueba.java
@@ -0,0 +1,13 @@
+package com.estopacomplementos.core.business;
+
+public class Prueba {
+	
+	public static void main(String[] args) {
+		String numero = "1989@56";
+		String cantidad[] = numero.split("@");
+		String entero = cantidad[0];
+		String decimal = cantidad[1];
+		
+	}
+
+}
